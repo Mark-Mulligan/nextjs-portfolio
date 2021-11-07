058a731eaab025c905d5be66e6caadbd6df28489
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-11-07 13:32:32 -0600

    created navbar and quote for hero section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import Image from 'next/image';
 
 import styles from '../styles/Home.module.scss';
 
@@ -12,7 +13,50 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}></main>
+      <main className={styles.main}>
+        <section className={styles.heroSection}>
+          <nav className={styles.navbar}>
+            <ul className={styles.pageLinks}>
+              <li>
+                <Image height={50} width={50} src="/icons/website-logo.png" alt="M Logo" />
+              </li>
+              <li>
+                <a href="#">Projects</a>
+              </li>
+              <li>
+                <a href="#">Skills</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+
+            <ul className={styles.navSocialLinks}>
+              <li>
+                <a>
+                  <Image height={50} width={50} src="/icons/githubWhite.svg" alt="github link" />
+                </a>
+              </li>
+              <li>
+                <a>
+                  <Image height={45} width={45} src="/icons/linkedin.svg" alt="Linkedin Link" />
+                </a>
+              </li>
+            </ul>
+          </nav>
+
+          <div className={styles.quoteContainer}>
+            <p className={styles.quoteText}>
+              Making the simple complicated is commonplace. Making the complicated simple, that&#39;s creativity.
+            </p>
+            <p className={styles.quoteAuthor}>Charles Mingus</p>
+            <hr />
+          </div>
+        </section>
+      </main>
     </div>
   );
 };
