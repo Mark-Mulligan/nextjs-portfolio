85953d8c0978030b0b2b2bef28032690c79a96dd
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-12-28 23:49:57 -0600

    added to project section

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -9,14 +9,35 @@ const ProjectSection = () => {
         <div className={styles.leftCol}>
           <div className={styles.projectWrapper}>
             <Image src="/images/betterMusic.jpg" layout="fill" objectFit="cover" alt="Picture of beats headphones" />
+            <div className={styles.projectInfoContainer}>
+              <div className={styles.projectInfo}>
+                <h3>Better Music</h3>
+                <p>Front End - UI</p>
+                <button>Launch Project</button>
+              </div>
+            </div>
           </div>
         </div>
         <div className={styles.rightCol}>
           <div className={styles.projectWrapper}>
             <Image src="/images/outdoorAdventures.jpg" layout="fill" objectFit="cover" alt="Mountains and trees view" />
+            <div className={styles.projectInfoContainer}>
+              <div className={styles.projectInfo}>
+                <h3>Outdoor Adventures</h3>
+                <p>Full Stack - React, Redux, Node.js, MySQL</p>
+                <button>Launch Project</button>
+              </div>
+            </div>
           </div>
           <div className={styles.projectWrapper}>
             <Image src="/images/InvoiceGenerator.jpg" layout="fill" objectFit="cover" alt="Computer with invoices" />
+            <div className={styles.projectInfoContainer}>
+              <div className={styles.projectInfo}>
+                <h3>Invoice Generator</h3>
+                <p>Full Stack - React, Node.js, MongoDB</p>
+                <button>Launch Project</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
