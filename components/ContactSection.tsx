3a1cced2ca9ea6b81713879d941b3980029fd68f
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-12-31 10:02:22 -0600

    worked on contact section

--- components/ContactSection.tsx
+++ components/ContactSection.tsx
@@ -42,6 +42,10 @@ const ContactSection = () => {
           </li>
         </div>
       </ul>
+      <div className={styles.email}>
+        <p>Interest in reaching out to me? Send me an email:</p>
+        <a>mark.mulligan.jr1@gmail.com</a>
+      </div>
     </section>
   );
 };
