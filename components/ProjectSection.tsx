61fc72d2ff582ccda5f8590ac36d8697011ff36c
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-12-30 20:05:02 -0600

    changed button on projects to more info

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -10,27 +10,17 @@ const ProjectSection = () => {
         <div className={styles.leftCol}>
           <div className={styles.projectWrapper}>
             <Image src="/images/betterMusic.jpg" layout="fill" objectFit="cover" alt="Picture of beats headphones" />
-            <ProjectLabel title="Better Music" subTitle="Front End - UI" btnText="Launch Project" projectLink="#" />
+            <ProjectLabel title="Better Music" subTitle="Front End - UI" />
           </div>
         </div>
         <div className={styles.rightCol}>
           <div className={styles.projectWrapper}>
             <Image src="/images/outdoorAdventures.jpg" layout="fill" objectFit="cover" alt="Mountains and trees view" />
-            <ProjectLabel
-              title="Outdoor Adventures"
-              subTitle="Full Stack - React, Redux, Node.js, MySQL"
-              btnText="Launch Project"
-              projectLink="#"
-            />
+            <ProjectLabel title="Outdoor Adventures" subTitle="Full Stack" />
           </div>
           <div className={styles.projectWrapper}>
             <Image src="/images/InvoiceGenerator.jpg" layout="fill" objectFit="cover" alt="Computer with invoices" />
-            <ProjectLabel
-              title="Invoice generator"
-              subTitle="Full Stack - React, Node.js, MongoDB"
-              btnText="Launch Project"
-              projectLink="#"
-            />
+            <ProjectLabel title="Invoice generator" subTitle="Full Stack" />
           </div>
         </div>
       </div>
--- components/ui/ProjectLabel.tsx
+++ components/ui/ProjectLabel.tsx
@@ -1,53 +1,17 @@
-import Image from 'next/image';
 import styles from '../../styles/ProjectLabel.module.scss';
-import { useEffect, useState, useRef } from 'react';
 
 interface ProjectLabelProps {
   title: string;
   subTitle: string;
-  btnText: string;
-  projectLink: string;
 }
 
-const ProjectLabel = ({ title, subTitle, btnText, projectLink }: ProjectLabelProps) => {
-  const [isHovering, setIsHovering] = useState(false);
-  const ref = useRef<null | HTMLAnchorElement>(null);
-
-  const handleMouseOver = () => setIsHovering(true);
-  const handleMouseOut = () => setIsHovering(false);
-
-  useEffect(
-    () => {
-      const node = ref.current;
-
-      if (node && node !== null) {
-        node.addEventListener('mouseover', handleMouseOver);
-        node.addEventListener('mouseout', handleMouseOut);
-        return () => {
-          node.removeEventListener('mouseover', handleMouseOver);
-          node.removeEventListener('mouseout', handleMouseOut);
-        };
-      }
-    },
-    [], // Recall only if ref changes
-  );
-
+const ProjectLabel = ({ title, subTitle }: ProjectLabelProps) => {
   return (
     <div className={styles.projectInfoContainer}>
       <div className={styles.projectInfo}>
         <h3>{title}</h3>
         <p>{subTitle}</p>
-        <a ref={ref} href={projectLink}>
-          <span>{btnText}</span>
-          <span className={styles.linkArrow}>
-            <Image
-              src={isHovering ? '/icons/right-arrow.png' : '/icons/white-right-arrow.png'}
-              alt="link to project page in another tab"
-              height={15}
-              width={15}
-            />
-          </span>
-        </a>
+        <button>More Info</button>
       </div>
     </div>
   );
