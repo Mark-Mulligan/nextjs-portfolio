d4d1461435929429ea4c127fe7f178dce0ec5fa8
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2022-08-22 12:14:22 -0500

    moved experiences

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -8,23 +8,6 @@ const Experience = () => {
           <h4>Full Stack Developer</h4>
         </div>
         <div className={styles.rightCol}>
-          <h6>Visual Storage Intelligence</h6>
-          <p>June 2021 - February 2022</p>
-          <ul>
-            <li>
-              Python / React / Node.js / MSSQL developement engineer for an application that provides in-depth
-              enterprise storage analytics
-            </li>
-            <li>Developed two factor authentication system for the application</li>
-          </ul>
-        </div>
-      </div>
-
-      <div className={styles.row}>
-        <div className={styles.leftCol}>
-          <h4>Full Stack Developer</h4>
-        </div>
-        <div className={styles.rightCol}>
           <h6>Evolon</h6>
           <p>February 2022 - Present</p>
           <ul>
@@ -32,8 +15,24 @@ const Experience = () => {
               Typescript / React / Node.js development for a product that leverages AI to make security cameras more
               intelligent
             </li>
-            <li>Responsible for modernizing UI/UX design on legacy applcations</li>
+            <li>Responsible for modernizing UI/UX design on legacy applications</li>
             <li>Assisted with architecture and design of a brand new enterprise software</li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.row}>
+        <div className={styles.leftCol}>
+          <h4>Full Stack Developer</h4>
+        </div>
+        <div className={styles.rightCol}>
+          <h6>Visual Storage Intelligence</h6>
+          <p>June 2021 - February 2022</p>
+          <ul>
+            <li>
+              Python / React / Node.js / MSSQL development engineer for an application that provides in-depth enterprise
+              storage analytics
+            </li>
+            <li>Developed two factor authentication system for the application</li>
           </ul>
         </div>
       </div>
